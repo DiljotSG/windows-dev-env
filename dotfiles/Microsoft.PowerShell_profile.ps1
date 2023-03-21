d31eecd62b63375a1c6972535e8034f435d2135e
Diljot Garcha <19293725+DiljotSG@users.noreply.github.com>
2023-03-20 20:55:37 -0500

    Update Microsoft.PowerShell_profile.ps1

--- dotfiles/Microsoft.PowerShell_profile.ps1
+++ dotfiles/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 # Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/star.omp.json")
-Invoke-Expression (oh-my-posh --init --shell pwsh --config "~/.star.omp.json")
+# Invoke-Expression (oh-my-posh --init --shell pwsh --config "~/.star.omp.json")
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/star.omp.json" | Invoke-Expression
 
 function to-git
 {
